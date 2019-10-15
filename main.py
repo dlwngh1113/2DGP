0703d0fd47987208052b5ce8d61f5dc6c5393390
dlwngh1113 <dlwngh1113@naver.com>
2019-10-15 19:59:42 +0900

    player class added

--- main.py
+++ main.py
@@ -1,53 +1,37 @@
-from lib2to3.fixer_util import p1
-
 from pico2d import *
 
 Width, Height = 550, 750
-frame = 0
-charWidth = 41
-charHeight = 51
 open_canvas(Width, Height)
 main_page = load_image('main page.png')
 
 
-class player:
-    image = load_image('archer.png')
-    x, y = 300, 300
-    dir = 0
-    vel = 30
+class Player:
+    def __init__(self):
+        self.image = load_image('archer.png')
+        self.x, self.y = 300, 300
+        self.dir = 0
+        self.vel = 30
+        self.charWidth = 41
+        self.charHeight = 51
+        self.xframe, self.yframe = 0, 0
 
-    def move(self):
-        events = get_events()
-        for event in events:
-            if event.type == SDL_KEYDOWN:
-                if event.key == SDLK_LEFT:
-                    player.dir -= 1
-                    player.x += player.dir * player.vel
-                elif event.key == SDLK_RIGHT:
-                    player.dir += 1
-                    player.x += player.dir * player.vel
-                elif event.key == SDLK_UP:
-                    player.y += player.vel
-                elif event.key == SDLK_DOWN:
-                    player.y -= player.vel
-            elif event.type == SDL_KEYUP:
-                if event.key == SDLK_RIGHT:
-                    player.dir -= 1
-                elif event.key == SDLK_LEFT:
-                    player.dir += 1
+    def draw(self):
+        self.image.clip_draw_to_origin(self.charWidth * self.xframe, self.charHeight * self.yframe, self.charWidth,
+                                       self.charHeight, self.x, self.y, self.charWidth * 1.5, self.charHeight * 1.5)
+
+    def update(self):
+        self.yframe = (self.yframe + 1) % 6
 
 
-# item_page = load_image('item page.png')
-p1 = player()
+player = Player()
 
 while True:
     clear_canvas()
     main_page.clip_draw(0, 0, Width, Height, Width / 2, Height / 2)
-    p1.image.clip_draw_to_origin(charWidth * 4, charHeight * frame, charWidth, charHeight, p1.x, p1.y, charWidth * 1.5,
-                                  charHeight * 1.5)
-    frame = (frame + 1) % 5 + 2
+    player.draw()
+
+    player.update()
     update_canvas()
-    p1.move()
     delay(0.1)
 
 close_canvas()
