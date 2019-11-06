97f7e29a6e9b7ee3ec9cdacff8d5e68702630c5f
dlwngh1113 <dlwngh1113@naver.com>
2019-11-07 01:14:24 +0900

    file i/o put able

--- codes/maptool.py
+++ codes/maptool.py
@@ -1,29 +1,62 @@
 from pico2d import *
 import math
 
-open_canvas(550, 750)
+WIDTH, HEIGHT = 550, 750
+open_canvas(WIDTH, HEIGHT)
 map_list = []
+SIZE = 50
 
 
 def load():
+    global map_list, HEIGHT, SIZE
+    filename = 'map1.txt'
+    with open(filename) as data:
+        map_list = [[int(i) for i in line.split()] for line in data.readlines()]
+    print_ary()
     pass
+
+
+def load_stage(filename):
+    with open(filename) as data:
+        map_source = [[int(i) for i in line.split()] for line in data.readlines()]
+    return map_source
 
 
 def save():
+    global map_list
+    f = open('map1.txt', 'w')
+    for i in range(len(map_list)):
+        for j in range(len(map_list[i])):
+            f.write(str(map_list[i][j]))
+            f.write(' ')
+        f.write('\n')
+    f.close()
+    print('successfully saved!')
     pass
 
 
+def print_ary():
+    global map_list
+    for i in range(len(map_list)):
+        for j in range(len(map_list[i])):
+            print(map_list[i][j], end=' ')
+        print()
+
+
 def handle_events():
+    global SIZE, map_list, WIDTH, HEIGHT
     events = get_events()
     for event in events:
         if event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
-            print('sibak')
+            map_list[event.y // SIZE][event.x // SIZE] = 1
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_s:
             save()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_l:
             load()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
+            print_ary()
     pass
 
 
