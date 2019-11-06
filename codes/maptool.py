8928fcf3f802b2bba6079a3f079a1bdc5a985c3a
dlwngh1113 <dlwngh1113@naver.com>
2019-11-06 23:58:15 +0900

    maptool function added

--- codes/maptool.py
+++ codes/maptool.py
@@ -12,7 +12,17 @@ def save():
 def handle_events():
     events = get_events()
     for event in events:
-        if event.x // 50
+        if event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            print('sibak')
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_s:
+            save()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_l:
+            load()
+    pass
+
+def draw():
     pass
 
 while True:
