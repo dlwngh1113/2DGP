e8139db9c752e46a02b6334e1e5ab8c5e2173f54
dlwngh1113 <dlwngh1113@naver.com>
2019-11-07 22:58:57 +0900

    maptool draw added

--- codes/maptool.py
+++ codes/maptool.py
@@ -5,7 +5,16 @@ WIDTH, HEIGHT = 550, 750
 open_canvas(WIDTH, HEIGHT)
 map_list = []
 SIZE = 50
-image = load_image('C:\\Users\\dlwng\\Desktop\\2DGP\\TermProj\\image_resources\\tile_sheet.png')
+select_num = 1
+tile_image = load_image('C:\\Users\\dlwng\\Desktop\\2DGP\\TermProj\\image_resources\\tile_sheet.png')
+
+
+class Map:
+    map_source = None
+
+    def __init__(self, filename):
+        with open(filename) as data:
+            self.map_source = [[int(i) for i in line.split()] for line in data.readlines()]
 
 
 def load():
@@ -45,11 +54,15 @@ def print_ary():
 
 
 def handle_events():
-    global SIZE, map_list, WIDTH, HEIGHT
+    global SIZE, map_list, WIDTH, HEIGHT, select_num
     events = get_events()
     for event in events:
         if event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
-            map_list[event.y // SIZE][event.x // SIZE] = 1
+            map_list[event.y // SIZE][event.x // SIZE] = select_num
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_MIDDLE:
+            select_num = (select_num + 1) % 14
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_RIGHT:
+            map_list[event.y // SIZE][event.x // SIZE] = 0
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_s:
@@ -62,6 +75,13 @@ def handle_events():
 
 
 def draw():
+    global map_list, tile_image, SIZE, HEIGHT
+    clear_canvas()
+    for i in range(len(map_list)):
+        for j in range(len(map_list[i])):
+            if map_list[i][j] != 0:
+                tile_image.clip_draw(0, map_list[i][j] * 32, 32, 32, (j + 0.5) * SIZE, (len(map_list) - i) * SIZE - SIZE // 2, SIZE, SIZE)
+    update_canvas()
     pass
 
 
@@ -78,3 +98,4 @@ def init():
 init()
 while True:
     handle_events()
+    draw()
