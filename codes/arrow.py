87029aa35c4945853992fec63c2397c8af8733e0
dlwngh1113 <dlwngh1113@naver.com>
2019-11-08 18:25:58 +0900

    arrow fire polymorphism

--- codes/arrow.py
+++ codes/arrow.py
@@ -11,13 +11,16 @@ class Arrow:
         self.target_x = event.x
         self.target_y = 750 - event.y
         self.velocity = 0
-        if()
+        self.gradient = (self.target_x - self.x) / (self.target_y - self.y)
+        if -1 < self.gradient < 1:
+            self.frame = 4
+        else:
+            self.frame = 2
         if self.image == None:
             self.image = load_image('C:\\Users\\dlwng\\Desktop\\2DGP\\TermProj\\image_resources\\arrow_image.png')
 
     def draw(self):
-        self.image.clip_draw(110, 0, 55, 48, self.x, self.y)
-        #self.image.clip_composite_draw()
+        self.image.clip_draw(55 * self.frame, 0, 55, 48, self.x, self.y)
         pass
 
     def update(self):
